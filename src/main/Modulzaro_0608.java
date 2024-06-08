67d78b283d9c23caa70a04e1e68f11188d9dcc89
Balazs Fajd <mrfajd@gmail.com>
2024-06-08 11:14:10 +0200

    void adatszerkezetTomb(), tomb2Eloallito(), tomb2Kiir(), tomb2Invertalo() | kiiras() javitva

--- src/main/Modulzaro_0608.java
+++ src/main/Modulzaro_0608.java
@@ -10,9 +10,10 @@ public class Modulzaro_0608 {
     static Random rnd = new Random();
     
     private static int[] tomb;
+    private static int[][] tomb2;
     private static final String SEP = System.lineSeparator();
-    private static final int alsoErtek = -10;
-    private static final int felsoErtek = 10;
+    private static final int alsoErtek = -50;
+    private static final int felsoErtek = 50;
     
     public static void main(String[] args) {
         modulzaro();
@@ -20,6 +21,7 @@ public class Modulzaro_0608 {
 
     private static void modulzaro() {
         programozoiTetelek();
+        adatszerkezetTomb();
     }
     
     private static void programozoiTetelek() {
@@ -35,6 +37,90 @@ public class Modulzaro_0608 {
         eldontesMindBemutatasa();
         linKerMindBemutatasa();
     }
+    
+    private static void adatszerkezetTomb() {
+        tomb2Eloallito(3);
+        tomb2kiir(tomb2);
+        
+        tomb2 = tomb2Invertalo(tomb2);
+        tomb2kiir(tomb2);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public static int[][] tomb2Eloallito(int meret) {
+        tomb2 = new int[meret][meret];
+        for (int i = 0; i < meret; i++) {
+            for (int j = 0; j < meret; j++) {
+                if (i == j) {
+                    tomb2[i][j] = velSzam(alsoErtek, felsoErtek);
+                } else {
+                    tomb2[i][j] = 0;
+                }
+            }
+        }
+        return tomb2;
+    }
+    
+    public static void tomb2kiir(int[][] atvettTomb) {
+        System.out.print("\t");
+        for (int j = 0; j < atvettTomb.length; j++) {
+            System.out.print("\u001B[33m" + (j + 1) + "\t");
+        }
+        System.out.println();
+        for (int i = 0; i < atvettTomb.length; i++) {
+            System.out.print("\u001B[33m" + (i + 1) + "\t");
+            for (int j = 0; j < atvettTomb[i].length; j++) {
+                System.out.print("\u001B[0m" + atvettTomb[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static int[][] tomb2Invertalo(int[][] atvettTomb) {
+        int[][] ujtomb = atvettTomb;
+        for (int i = 0; i < atvettTomb.length; i++) {
+            for (int j = 0; j < atvettTomb.length; j++) {
+                if (i == j) {
+                    ujtomb[i][j] = 0;
+                } else {
+                    ujtomb[i][j] = (tomb2Min() + tomb2Max()) / 2;
+                }
+            }
+        }
+        return ujtomb;
+    }
+    
+    private static int tomb2Min() {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < tomb2.length; i++) {
+            for (int j = 0; j < tomb2[i].length; j++) {
+                if (tomb2[i][j] < min) {
+                    min = tomb2[i][j];
+                }
+            }
+        }
+        return min;
+    }
+    
+    private static int tomb2Max() {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < tomb2.length; i++) {
+            for (int j = 0; j < tomb2[i].length; j++) {
+                if (tomb2[i][j] > max) {
+                    max = tomb2[i][j];
+                }
+            }
+        }
+        return max;
+    }
+    
+    
    
     
     
@@ -61,7 +147,7 @@ public class Modulzaro_0608 {
     }
     
     public static void kivalasztasBemutatasa() {
-        konzol("[Kiválasztás]: A tömbben lévő első 5-tel osztható szám helyét adja vissza: " + kivalasztas() + SEP);
+        konzol("[Kiválasztás]: A tömbben lévő első 5-tel osztható szám helyét adja vissza (ha nincs, mert véletlenszerű számok vannak a tömbben, akkor '-1'): " + kivalasztas() + SEP);
     }
     
     public static void eldontesEgyBemutatasa() {
@@ -100,7 +186,7 @@ public class Modulzaro_0608 {
     }
     
     private static int minHely() {
-       int minIndex = 0;
+        int minIndex = 0;
         for (int i = 1; i < tomb.length; i++) {
             if(tomb[i] < tomb[minIndex]){
                 minIndex = i;
@@ -120,11 +206,12 @@ public class Modulzaro_0608 {
     }
     
     private static int kivalasztas() {
-        int i = 0;
-        while(!(tomb[i] % 5 == 0)){
-            i++;
-        }
-        return i;  
+        for (int i = 0; i < tomb.length; i++) {
+            if(tomb[i] % 5 == 0) {
+                return i;
+            }
+        } 
+        return -1;
     }
     
     private static boolean eldontesEgy() {
@@ -160,6 +247,10 @@ public class Modulzaro_0608 {
     
     
     
+
+    public static double mertaniKozep(double min, double max) {
+        return Math.sqrt(min * max);
+    }
     
     public static boolean tokeletesSzam(int szam) {
         int osztoOsszeg = 0;
